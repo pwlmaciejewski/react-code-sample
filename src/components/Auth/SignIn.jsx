5c30035281ec173383911a27e4c0f1958948e470
Paweł Maciejewski <pwlmaciejewski@gmail.com>
2016-12-28 15:49:53 +0100

    Create auth reducer.

--- src/components/Auth/SignIn.jsx
+++ src/components/Auth/SignIn.jsx
@@ -1,16 +1,53 @@
 import { Component } from 'react';
 import { connect } from 'react-redux';
+import { autobind } from 'core-decorators';
 import classNames from 'classnames/bind';
 import styles from './SignIn.scss';
+import _ from 'lodash';
 import TextField from 'material-ui/TextField';
 import AppBar from 'material-ui/AppBar';
 import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
+import {
+  requestSignIn,
+  resetSignIn
+} from '../../modules/auth';
 
 const cx = classNames.bind(styles);
 
+@connect(
+  state => state.auth,
+  ({
+    requestSignIn,
+    resetSignIn
+  })
+)
+@autobind
 export default class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+  componentWillMount() {
+    this.props.resetSignIn();
+  }
+  onEmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
+  onPasswordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+  onSubmit(event) {
+    event.preventDefault();
+    const { email, password } = this.state;
+    this.props.requestSignIn(email, password);
+  }
   render() {
+    const { pending } = this.props;
+    const { email, password } = this.state;
     return (
       <div className={ cx('root') }>
         <AppBar
@@ -18,24 +55,32 @@ export default class SignIn extends Component {
           showMenuIconButton={ null }
         />
         <Paper className={ cx('content') }>
-          <form>
+          <form onSubmit={ this.onSubmit }>
             <TextField
+              disabled={ pending }
               className={ cx('field') }
               type={ 'email' }
-              floatingLabelText={ 'Login' }
+              floatingLabelText={ 'E-mail' }
+              value={ email }
+              onChange={ this.onEmailChange }
               floatingLabelFixed
               autoFocus
             />
             <TextField
+              disabled={ pending }
               className={ cx('field') }
               type={ 'password' }
+              value={ password }
+              onChange={ this.onPasswordChange }
               floatingLabelText={ 'Password' }
               floatingLabelFixed
             />
             <div className={ cx('actions-wrapper') }>
               <RaisedButton
+                label={ pending ? 'Please wait...' : 'Login' }
+                disabled={ pending }
+                type={ 'submit' }
                 primary
-                label={ 'Login' }
               />
             </div>
           </form>
--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,7 +1,56 @@
-const initialState = {};
+export const REQUEST_SIGN_IN = 'REQUEST_SIGN_IN';
+export const requestSignIn = (email, password) => ({
+  type: REQUEST_SIGN_IN,
+  payload: { email, password }
+});
+
+export const SIGN_IN_SUCCESS = 'SIGN_IN_SUCCESS';
+export const signInSuccess = () => ({
+  type: SIGN_IN_SUCCESS
+});
+
+export const SIGN_IN_FAIL = 'SIGN_IN_FAIL';
+export const signInFail = err => ({
+  type: SIGN_IN_FAIL,
+  payload: err
+});
+
+export const RESET_SIGN_IN = 'RESET_SIGN_IN';
+export const resetSignIn = () => ({
+  type: RESET_SIGN_IN
+});
+
+const initialState = {
+  pending: false,
+  error: null,
+  success: false
+};
 
 export default function reducer(state = initialState, { type, payload }) {
   switch (type) {
+    case REQUEST_SIGN_IN:
+      return {
+        ...state,
+        pending: true,
+        error: null,
+        success: false
+      };
+    case SIGN_IN_SUCCESS:
+      return {
+        ...state,
+        pending: false,
+        error: null,
+        success: true
+      };
+    case SIGN_IN_FAIL:
+      return {
+        ...state,
+        pending: false,
+        error: payload,
+        success: false
+      };
+    case RESET_SIGN_IN:
+      return { ...initialState };
     default:
       return state;
   }
